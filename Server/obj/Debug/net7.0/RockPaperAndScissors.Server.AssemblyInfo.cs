cd6775e53b5c15567e762ef63b69b921d04b09f8
Ibraheem-MuhammedAmeen <ibraheemameen941@gmail.com>
2024-06-20 18:07:55 +0100

    first commit

--- Server/obj/Debug/net7.0/RockPaperAndScissors.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net7.0/RockPaperAndScissors.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RockPaperAndScissors.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821c40970fc05b5121079c72346d11c889e7cc54")]
 [assembly: System.Reflection.AssemblyProductAttribute("RockPaperAndScissors.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RockPaperAndScissors.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
